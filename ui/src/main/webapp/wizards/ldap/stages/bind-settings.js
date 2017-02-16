bd6a9d4991bdaa908accfcb185b1e2fb10b46dd8
Garrett Freibott <garrett.freibott@connexta.com>
2017-02-16 12:31:03 -0700

    DDF-2773: Fixed Spinner Alignment Bug
     - Moved Spinner into the Stage class to clean up wizard jsx
       and keep styles in one place

--- ui/src/main/webapp/wizards/ldap/stages/bind-settings.js
+++ ui/src/main/webapp/wizards/ldap/stages/bind-settings.js
@@ -8,7 +8,6 @@ import Description from 'components/Description'
 import Action from 'components/Action'
 import ActionGroup from 'components/ActionGroup'
 import Message from 'components/Message'
-import Spinner from 'components/Spinner'
 
 import {
   Input,
@@ -41,59 +40,57 @@ const BindSettings = (props) => {
   }
 
   return (
-    <Stage>
+    <Stage submitting={submitting}>
       <Mount
         on={setDefaults}
         bindUser={ldapType === 'activeDirectory' ? 'user@domain' : 'cn=admin'}
         bindUserPassword='secret'
         bindUserMethod='Simple' />
 
-      <Spinner submitting={submitting}>
-        <Title>LDAP Bind User Settings</Title>
-        <Description>
-          In order for the system to retrieve information from the LDAP store, it needs to bind
-          a user that has permission to search it. This user will be used by the system whenever
-          it needs to access the LDAP store.
-        </Description>
-        <Description> {/* todo - left justify this one? maybe not - test if it looks bad */}
-          User credentials may be provided as a DN (distinguished name), User ID, or in the format UserID@Realm.
-          The requirements for different LDAP servers vary; please contact your LDAP administrator if you need guidance.
-        </Description>
+      <Title>LDAP Bind User Settings</Title>
+      <Description>
+        In order for the system to retrieve information from the LDAP store, it needs to bind
+        a user that has permission to search it. This user will be used by the system whenever
+        it needs to access the LDAP store.
+      </Description>
+      <Description> {/* todo - left justify this one? maybe not - test if it looks bad */}
+        User credentials may be provided as a DN (distinguished name), User ID, or in the format UserID@Realm.
+        The requirements for different LDAP servers vary; please contact your LDAP administrator if you need guidance.
+      </Description>
 
-        <Input id='bindUser' disabled={disabled} label='Bind User' />
-        <Password id='bindUserPassword' disabled={disabled} label='Bind User Password' />
-        <Select id='bindUserMethod'
-          label='Bind User Method'
+      <Input id='bindUser' disabled={disabled} label='Bind User' />
+      <Password id='bindUserPassword' disabled={disabled} label='Bind User Password' />
+      <Select id='bindUserMethod'
+        label='Bind User Method'
+        disabled={disabled}
+        options={bindUserMethodOptions} />
+      {/* removed options: 'SASL', 'GSSAPI SASL' */}
+      {/* TODO GSSAPI SASL only */}
+      {/* <Input id='bindKdcAddress' disabled={disabled} label='KDC Address (for Kerberos authentication)' /> */}
+      {/* TODO GSSAPI and Digest MD5 SASL only */}
+      {/* Realm is needed for Kerberos and MD5 auth, currently only MD5 is supported by the wizard */}
+      {
+        (bindUserMethod === 'Digest MD5 SASL')
+          ? (<Input id='bindRealm' disabled={disabled} label='Realm (for Digest MD5 authentication)' />)
+          : null
+      }
+
+      <ActionGroup>
+        <Action
+          secondary
+          label='back'
+          onClick={prev}
+          disabled={disabled} />
+        <Action
+          primary
+          label='next'
+          onClick={test}
           disabled={disabled}
-          options={bindUserMethodOptions} />
-        {/* removed options: 'SASL', 'GSSAPI SASL' */}
-        {/* TODO GSSAPI SASL only */}
-        {/* <Input id='bindKdcAddress' disabled={disabled} label='KDC Address (for Kerberos authentication)' /> */}
-        {/* TODO GSSAPI and Digest MD5 SASL only */}
-        {/* Realm is needed for Kerberos and MD5 auth, currently only MD5 is supported by the wizard */}
-        {
-          (bindUserMethod === 'Digest MD5 SASL')
-            ? (<Input id='bindRealm' disabled={disabled} label='Realm (for Digest MD5 authentication)' />)
-            : null
-        }
+          nextStageId='directory-settings'
+          testId='bind' />
+      </ActionGroup>
 
-        <ActionGroup>
-          <Action
-            secondary
-            label='back'
-            onClick={prev}
-            disabled={disabled} />
-          <Action
-            primary
-            label='next'
-            onClick={test}
-            disabled={disabled}
-            nextStageId='directory-settings'
-            testId='bind' />
-        </ActionGroup>
-
-        {messages.map((msg, i) => <Message key={i} {...msg} />)}
-      </Spinner>
+      {messages.map((msg, i) => <Message key={i} {...msg} />)}
     </Stage>
   )
 }
--- ui/src/main/webapp/wizards/ldap/stages/confirm.js
+++ ui/src/main/webapp/wizards/ldap/stages/confirm.js
@@ -8,7 +8,6 @@ import Description from 'components/Description'
 import Action from 'components/Action'
 import ActionGroup from 'components/ActionGroup'
 import Info from 'components/Information'
-import Spinner from 'components/Spinner'
 import Message from 'components/Message'
 import MapDisplay from 'components/MapDisplay'
 
@@ -21,60 +20,58 @@ const useCaseMapping = {
 }
 
 export default ({ disabled, prev, persist, submitting, messages = [], configs }) => (
-  <Stage>
-    <Spinner submitting={submitting}>
-      <Title>LDAP Settings Confirmation</Title>
+  <Stage submitting={submitting}>
+    <Title>LDAP Settings Confirmation</Title>
 
-      <Description>
-        All of the values have been successfully verified. Would you like to
-        save the LDAP configuration?
-      </Description>
+    <Description>
+      All of the values have been successfully verified. Would you like to
+      save the LDAP configuration?
+    </Description>
 
-      <Flexbox flexDirection='column'>
-        <Flexbox flexDirection='row' justifyContent='space-between'>
-          <Flexbox className={confirmationInfo} flexDirection='column'>
-            <Info label='LDAP Function' value={useCaseMapping[configs.ldapUseCase]} />
-            <Info label='Hostname' value={configs.hostName} />
-            <Info label='Port' value={configs.port} />
-            <Info label='Encryption Method' value={configs.encryptionMethod} />
-            <Info label='Base User DN' value={configs.baseUserDn} />
-            <Info label='User Name Attribute' value={configs.userNameAttribute} />
-          </Flexbox>
-          <Flexbox className={confirmationInfo} flexDirection='column'>
-            <Info label='Base Group DN' value={configs.baseGroupDn} />
-            <Info label='Bind User' value={configs.bindUser} />
-            <Info label='Bind User Password' value='*****' />
-            <Info label='Bind User Method' value={configs.bindUserMethod} />
-            <Info label='LDAP Group Object Class' value={configs.groupObjectClass} />
-            <Info label='Group Attribute Holding Member References'
-              value={configs.groupAttributeHoldingMember} />
-            <Info label='Member Attribute Referenced in Groups'
-              value={configs.memberAttributeReferencedInGroup} />
-          </Flexbox>
+    <Flexbox flexDirection='column'>
+      <Flexbox flexDirection='row' justifyContent='space-between'>
+        <Flexbox className={confirmationInfo} flexDirection='column'>
+          <Info label='LDAP Function' value={useCaseMapping[configs.ldapUseCase]} />
+          <Info label='Hostname' value={configs.hostName} />
+          <Info label='Port' value={configs.port} />
+          <Info label='Encryption Method' value={configs.encryptionMethod} />
+          <Info label='Base User DN' value={configs.baseUserDn} />
+          <Info label='User Name Attribute' value={configs.userNameAttribute} />
         </Flexbox>
-        { configs.ldapUseCase !== 'authentication'
-          ? (
-            <MapDisplay label='Attribute Mappings'
-              mapping={configs.attributeMappings} />
-          ) : null }
+        <Flexbox className={confirmationInfo} flexDirection='column'>
+          <Info label='Base Group DN' value={configs.baseGroupDn} />
+          <Info label='Bind User' value={configs.bindUser} />
+          <Info label='Bind User Password' value='*****' />
+          <Info label='Bind User Method' value={configs.bindUserMethod} />
+          <Info label='LDAP Group Object Class' value={configs.groupObjectClass} />
+          <Info label='Group Attribute Holding Member References'
+            value={configs.groupAttributeHoldingMember} />
+          <Info label='Member Attribute Referenced in Groups'
+            value={configs.memberAttributeReferencedInGroup} />
+        </Flexbox>
       </Flexbox>
+      { configs.ldapUseCase !== 'authentication'
+        ? (
+          <MapDisplay label='Attribute Mappings'
+            mapping={configs.attributeMappings} />
+        ) : null }
+    </Flexbox>
 
-      <ActionGroup>
-        <Action
-          secondary
-          label='back'
-          onClick={prev}
-          disabled={disabled} />
-        <Action
-          primary
-          label='save'
-          onClick={persist}
-          disabled={disabled}
-          persistId='create'
-          nextStageId='final-stage' />
-      </ActionGroup>
+    <ActionGroup>
+      <Action
+        secondary
+        label='back'
+        onClick={prev}
+        disabled={disabled} />
+      <Action
+        primary
+        label='save'
+        onClick={persist}
+        disabled={disabled}
+        persistId='create'
+        nextStageId='final-stage' />
+    </ActionGroup>
 
-      {messages.map((msg, i) => <Message key={i} {...msg} />)}
-    </Spinner>
+    {messages.map((msg, i) => <Message key={i} {...msg} />)}
   </Stage>
 )

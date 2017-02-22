8835ce0d3a687a7ff095a94e64ad597da0793333
Richard A. Porter <richard.porter@connexta.com>
2017-02-22 13:02:32 -0700

    DDF-2773 Adds checkstyle plugin for repository.

--- federation/source-config-handlers/src/main/java/org/codice/ddf/admin/sources/SourcesCommons.java
+++ federation/source-config-handlers/src/main/java/org/codice/ddf/admin/sources/SourcesCommons.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
 package org.codice.ddf.admin.sources;
 
 import static org.codice.ddf.admin.api.handler.commons.SourceHandlerCommons.PING_TIMEOUT;
@@ -15,8 +28,7 @@ import org.apache.http.ssl.SSLContexts;
 
 public class SourcesCommons {
 
-    public static CloseableHttpClient getCloseableHttpClient(boolean trustAnyCA)
-            throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+    public static CloseableHttpClient getCloseableHttpClient(boolean trustAnyCA) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         HttpClientBuilder builder = HttpClientBuilder.create().disableAutomaticRetries().setDefaultRequestConfig(
                 RequestConfig.custom().setConnectTimeout(PING_TIMEOUT).build());
         if (trustAnyCA) {
@@ -26,7 +38,6 @@ public class SourcesCommons {
         }
         return builder.build();
     }
-
 
     public static void closeClientAndResponse(CloseableHttpClient client, CloseableHttpResponse response) {
         try {

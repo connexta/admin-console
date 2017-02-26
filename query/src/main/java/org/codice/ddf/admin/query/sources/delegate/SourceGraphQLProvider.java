1ee89adfac7e33424cf39b2196f433bac43c96cc
tbatie <tracy.batie@connexta.com>
2017-02-26 11:49:13 -0700

    Mid commit of creating a sources action handler

--- query/src/main/java/org/codice/ddf/admin/query/sources/delegate/SourceGraphQLProvider.java
+++ query/src/main/java/org/codice/ddf/admin/query/sources/delegate/SourceGraphQLProvider.java
@@ -1,17 +1,21 @@
-package org.codice.ddf.admin.query.ldap;
+package org.codice.ddf.admin.query.sources.delegate;
 
 import static org.codice.ddf.admin.query.graphql.GraphQLCommons.handlerActionsToGraphQLFieldDefinition;
 import static org.codice.ddf.admin.query.graphql.GraphQLCommons.handlerToGraphQLObject;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
+
+import org.codice.ddf.admin.query.api.ActionHandler;
+import org.codice.ddf.admin.query.ldap.LdapActionHandler;
 
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
 import graphql.servlet.GraphQLMutationProvider;
 import graphql.servlet.GraphQLQueryProvider;
 
-public class LdapGraphQLProvider implements GraphQLQueryProvider, GraphQLMutationProvider {
+public class SourceGraphQLProvider implements GraphQLQueryProvider, GraphQLMutationProvider {
 
     @Override
     public Collection<GraphQLFieldDefinition> getMutations() {

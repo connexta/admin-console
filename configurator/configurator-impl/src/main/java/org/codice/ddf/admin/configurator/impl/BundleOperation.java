jefchng/admin-console-beta

DDF-2825 Breaks configurator services out into logical structure.

configurator/configurator-impl/src/main/java/org/codice/ddf/admin/configurator/impl/BundleOperation.java
@@ -13,11 +13,17 @@
  **/
 package org.codice.ddf.admin.configurator.impl;
 
+import static org.codice.ddf.admin.configurator.impl.ConfigValidator.validateString;
+import static org.codice.ddf.admin.configurator.impl.KarafUtils.getBundleContext;
+
 import java.util.Arrays;
 
 import org.apache.karaf.bundle.core.BundleState;
 import org.apache.karaf.bundle.core.BundleStateService;
 import org.codice.ddf.admin.configurator.ConfiguratorException;
+import org.codice.ddf.admin.configurator.Operation;
+import org.codice.ddf.admin.configurator.Result;
+import org.codice.ddf.internal.admin.configurator.actions.BundleActions;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -28,7 +34,26 @@ import org.slf4j.LoggerFactory;
 /**
  * Transactional handler for starting and stopping bundles.
  */
-public class BundleOperation implements OperationBase, Operation<Void, Boolean> {
+public class BundleOperation implements Operation<Void> {
+    public static class Actions implements BundleActions {
+        @Override
+        public BundleOperation start(String bundleSymName) throws ConfiguratorException {
+            validateString(bundleSymName, "Missing bundle name");
+            return new BundleOperation(bundleSymName, true, getBundleContext());
+        }
+
+        @Override
+        public BundleOperation stop(String bundleSymName) throws ConfiguratorException {
+            validateString(bundleSymName, "Missing bundle name");
+            return new BundleOperation(bundleSymName, false, getBundleContext());
+        }
+
+        @Override
+        public boolean isStarted(String bundleSymName) throws ConfiguratorException {
+            return start(bundleSymName).readState();
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(BundleOperation.class);
 
     private final boolean newState;
@@ -53,30 +78,8 @@ public class BundleOperation implements OperationBase, Operation<Void, Boolean> 
         initActivationState = lookupBundleState();
     }
 
-    /**
-     * Creates a handler that will start a bundle as part of a transaction.
-     *
-     * @param bundleSymName the name of the bundle to start
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static BundleOperation forStart(String bundleSymName, BundleContext bundleContext) {
-        return new BundleOperation(bundleSymName, true, bundleContext);
-    }
-
-    /**
-     * Creates a handler that will stop a bundle as part of a transaction.
-     *
-     * @param bundleSymName the name of the bundle to stop
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static BundleOperation forStop(String bundleSymName, BundleContext bundleContext) {
-        return new BundleOperation(bundleSymName, false, bundleContext);
-    }
-
     @Override
-    public Void commit() throws ConfiguratorException {
+    public Result<Void> commit() throws ConfiguratorException {
         try {
             if (initActivationState != newState) {
                 if (newState) {
@@ -90,11 +93,11 @@ public class BundleOperation implements OperationBase, Operation<Void, Boolean> 
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.pass();
     }
 
     @Override
-    public Void rollback() throws ConfiguratorException {
+    public Result<Void> rollback() throws ConfiguratorException {
         try {
             if (initActivationState != lookupBundleState()) {
                 if (initActivationState) {
@@ -108,11 +111,10 @@ public class BundleOperation implements OperationBase, Operation<Void, Boolean> 
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.rollback();
     }
 
-    @Override
-    public Boolean readState() throws ConfiguratorException {
+    Boolean readState() throws ConfiguratorException {
         return lookupBundleState();
     }
 

configurator/configurator-impl/src/main/java/org/codice/ddf/admin/configurator/impl/FeatureOperation.java
@@ -14,6 +14,8 @@
 package org.codice.ddf.admin.configurator.impl;
 
 import static org.apache.karaf.features.FeaturesService.Option.NoAutoRefreshBundles;
+import static org.codice.ddf.admin.configurator.impl.ConfigValidator.validateString;
+import static org.codice.ddf.admin.configurator.impl.KarafUtils.getBundleContext;
 
 import java.util.EnumSet;
 
@@ -21,6 +23,9 @@ import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeatureState;
 import org.apache.karaf.features.FeaturesService;
 import org.codice.ddf.admin.configurator.ConfiguratorException;
+import org.codice.ddf.admin.configurator.Operation;
+import org.codice.ddf.admin.configurator.Result;
+import org.codice.ddf.internal.admin.configurator.actions.FeatureActions;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
@@ -29,7 +34,26 @@ import org.slf4j.LoggerFactory;
 /**
  * Transactional handler for starting and stopping features.
  */
-public class FeatureOperation implements OperationBase, Operation<Void, Boolean> {
+public class FeatureOperation implements Operation<Void> {
+    public static class Actions implements FeatureActions {
+        @Override
+        public FeatureOperation start(String featureName) throws ConfiguratorException {
+            validateString(featureName, "Missing feature name");
+            return new FeatureOperation(featureName, true, getBundleContext());
+        }
+
+        @Override
+        public FeatureOperation stop(String featureName) throws ConfiguratorException {
+            validateString(featureName, "Missing feature name");
+            return new FeatureOperation(featureName, false, getBundleContext());
+        }
+
+        @Override
+        public boolean isFeatureStarted(String featureName) throws ConfiguratorException {
+            return start(featureName).readState();
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(FeatureOperation.class);
 
     private final String featureName;
@@ -48,30 +72,8 @@ public class FeatureOperation implements OperationBase, Operation<Void, Boolean>
         initActivationState = lookupFeatureStatus(getFeaturesService(), featureName);
     }
 
-    /**
-     * Creates a handler that will start a feature as part of a transaction.
-     *
-     * @param featureName   the name of the feature to start
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static FeatureOperation forStart(String featureName, BundleContext bundleContext) {
-        return new FeatureOperation(featureName, true, bundleContext);
-    }
-
-    /**
-     * Creates a handler that will stop a feature as part of a transaction.
-     *
-     * @param featureName   the name of the feature to stop
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static FeatureOperation forStop(String featureName, BundleContext bundleContext) {
-        return new FeatureOperation(featureName, false, bundleContext);
-    }
-
     @Override
-    public Void commit() throws ConfiguratorException {
+    public Result<Void> commit() throws ConfiguratorException {
         FeaturesService featuresService = getFeaturesService();
         try {
             if (initActivationState != newState) {
@@ -86,11 +88,11 @@ public class FeatureOperation implements OperationBase, Operation<Void, Boolean>
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.pass();
     }
 
     @Override
-    public Void rollback() throws ConfiguratorException {
+    public Result<Void> rollback() throws ConfiguratorException {
         FeaturesService featuresService = getFeaturesService();
         try {
             if (initActivationState != lookupFeatureStatus(featuresService, featureName)) {
@@ -105,11 +107,10 @@ public class FeatureOperation implements OperationBase, Operation<Void, Boolean>
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.rollback();
     }
 
-    @Override
-    public Boolean readState() throws ConfiguratorException {
+    Boolean readState() throws ConfiguratorException {
         return lookupFeatureStatus(getFeaturesService(), featureName);
     }
 

configurator/configurator-impl/src/main/java/org/codice/ddf/admin/configurator/impl/ServiceReaderImpl.java
@@ -1,0 +1,61 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.admin.configurator.impl;
+
+import static org.codice.ddf.admin.configurator.impl.KarafUtils.getBundleContext;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.codice.ddf.admin.configurator.ConfiguratorException;
+import org.codice.ddf.internal.admin.configurator.actions.ServiceReader;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <b> This code is experimental. While this interface is functional and tested, it may change or be
+ * removed in a future version of the library. </b>
+ */
+public class ServiceReaderImpl implements ServiceReader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceReaderImpl.class);
+
+    @Override
+    public <S> S getServiceReference(Class<S> serviceClass) throws ConfiguratorException {
+        BundleContext context = getBundleContext();
+        ServiceReference<S> ref = context.getServiceReference(serviceClass);
+        if (ref == null) {
+            return null;
+        }
+
+        return context.getService(ref);
+    }
+
+    @Override
+    public <S> Set<S> getServices(Class<S> serviceClass, String filter)
+            throws ConfiguratorException {
+        BundleContext context = getBundleContext();
+        try {
+            return context.getServiceReferences(serviceClass, filter)
+                    .stream()
+                    .map(context::getService)
+                    .collect(Collectors.toSet());
+        } catch (InvalidSyntaxException e) {
+            LOGGER.debug("Invalid filter [{}].", filter, e);
+            throw new ConfiguratorException(String.format("Received invalid filter [%s]", filter));
+        }
+    }
+}

connexta/admin-console

configurator/configurator-impl/src/main/java/org/codice/ddf/admin/configurator/impl/BundleOperation.java
@@ -13,11 +13,17 @@
  **/
 package org.codice.ddf.admin.configurator.impl;
 
+import static org.codice.ddf.admin.configurator.impl.ConfigValidator.validateString;
+import static org.codice.ddf.admin.configurator.impl.OsgiUtils.getBundleContext;
+
 import java.util.Arrays;
 
 import org.apache.karaf.bundle.core.BundleState;
 import org.apache.karaf.bundle.core.BundleStateService;
 import org.codice.ddf.admin.configurator.ConfiguratorException;
+import org.codice.ddf.admin.configurator.Operation;
+import org.codice.ddf.admin.configurator.Result;
+import org.codice.ddf.internal.admin.configurator.actions.BundleActions;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -28,7 +34,26 @@ import org.slf4j.LoggerFactory;
 /**
  * Transactional handler for starting and stopping bundles.
  */
-public class BundleOperation implements OperationBase, Operation<Void, Boolean> {
+public class BundleOperation implements Operation<Void> {
+    public static class Actions implements BundleActions {
+        @Override
+        public BundleOperation start(String bundleSymName) throws ConfiguratorException {
+            validateString(bundleSymName, "Missing bundle name");
+            return new BundleOperation(bundleSymName, true, getBundleContext());
+        }
+
+        @Override
+        public BundleOperation stop(String bundleSymName) throws ConfiguratorException {
+            validateString(bundleSymName, "Missing bundle name");
+            return new BundleOperation(bundleSymName, false, getBundleContext());
+        }
+
+        @Override
+        public boolean isStarted(String bundleSymName) throws ConfiguratorException {
+            return start(bundleSymName).readState();
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(BundleOperation.class);
 
     private final boolean newState;
@@ -53,30 +78,8 @@ public class BundleOperation implements OperationBase, Operation<Void, Boolean> 
         initActivationState = lookupBundleState();
     }
 
-    /**
-     * Creates a handler that will start a bundle as part of a transaction.
-     *
-     * @param bundleSymName the name of the bundle to start
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static BundleOperation forStart(String bundleSymName, BundleContext bundleContext) {
-        return new BundleOperation(bundleSymName, true, bundleContext);
-    }
-
-    /**
-     * Creates a handler that will stop a bundle as part of a transaction.
-     *
-     * @param bundleSymName the name of the bundle to stop
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static BundleOperation forStop(String bundleSymName, BundleContext bundleContext) {
-        return new BundleOperation(bundleSymName, false, bundleContext);
-    }
-
     @Override
-    public Void commit() throws ConfiguratorException {
+    public Result<Void> commit() throws ConfiguratorException {
         try {
             if (initActivationState != newState) {
                 if (newState) {
@@ -90,11 +93,11 @@ public class BundleOperation implements OperationBase, Operation<Void, Boolean> 
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.pass();
     }
 
     @Override
-    public Void rollback() throws ConfiguratorException {
+    public Result<Void> rollback() throws ConfiguratorException {
         try {
             if (initActivationState != lookupBundleState()) {
                 if (initActivationState) {
@@ -108,11 +111,10 @@ public class BundleOperation implements OperationBase, Operation<Void, Boolean> 
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.rollback();
     }
 
-    @Override
-    public Boolean readState() throws ConfiguratorException {
+    Boolean readState() throws ConfiguratorException {
         return lookupBundleState();
     }
 

configurator/configurator-impl/src/main/java/org/codice/ddf/admin/configurator/impl/FeatureOperation.java
@@ -14,6 +14,8 @@
 package org.codice.ddf.admin.configurator.impl;
 
 import static org.apache.karaf.features.FeaturesService.Option.NoAutoRefreshBundles;
+import static org.codice.ddf.admin.configurator.impl.ConfigValidator.validateString;
+import static org.codice.ddf.admin.configurator.impl.OsgiUtils.getBundleContext;
 
 import java.util.EnumSet;
 
@@ -21,6 +23,9 @@ import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeatureState;
 import org.apache.karaf.features.FeaturesService;
 import org.codice.ddf.admin.configurator.ConfiguratorException;
+import org.codice.ddf.admin.configurator.Operation;
+import org.codice.ddf.admin.configurator.Result;
+import org.codice.ddf.internal.admin.configurator.actions.FeatureActions;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
@@ -29,7 +34,26 @@ import org.slf4j.LoggerFactory;
 /**
  * Transactional handler for starting and stopping features.
  */
-public class FeatureOperation implements OperationBase, Operation<Void, Boolean> {
+public class FeatureOperation implements Operation<Void> {
+    public static class Actions implements FeatureActions {
+        @Override
+        public FeatureOperation start(String featureName) throws ConfiguratorException {
+            validateString(featureName, "Missing feature name");
+            return new FeatureOperation(featureName, true, getBundleContext());
+        }
+
+        @Override
+        public FeatureOperation stop(String featureName) throws ConfiguratorException {
+            validateString(featureName, "Missing feature name");
+            return new FeatureOperation(featureName, false, getBundleContext());
+        }
+
+        @Override
+        public boolean isFeatureStarted(String featureName) throws ConfiguratorException {
+            return start(featureName).readState();
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(FeatureOperation.class);
 
     private final String featureName;
@@ -48,30 +72,8 @@ public class FeatureOperation implements OperationBase, Operation<Void, Boolean>
         initActivationState = lookupFeatureStatus(getFeaturesService(), featureName);
     }
 
-    /**
-     * Creates a handler that will start a feature as part of a transaction.
-     *
-     * @param featureName   the name of the feature to start
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static FeatureOperation forStart(String featureName, BundleContext bundleContext) {
-        return new FeatureOperation(featureName, true, bundleContext);
-    }
-
-    /**
-     * Creates a handler that will stop a feature as part of a transaction.
-     *
-     * @param featureName   the name of the feature to stop
-     * @param bundleContext context needed for OSGi interaction
-     * @return instance of this class
-     */
-    public static FeatureOperation forStop(String featureName, BundleContext bundleContext) {
-        return new FeatureOperation(featureName, false, bundleContext);
-    }
-
     @Override
-    public Void commit() throws ConfiguratorException {
+    public Result<Void> commit() throws ConfiguratorException {
         FeaturesService featuresService = getFeaturesService();
         try {
             if (initActivationState != newState) {
@@ -86,11 +88,11 @@ public class FeatureOperation implements OperationBase, Operation<Void, Boolean>
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.pass();
     }
 
     @Override
-    public Void rollback() throws ConfiguratorException {
+    public Result<Void> rollback() throws ConfiguratorException {
         FeaturesService featuresService = getFeaturesService();
         try {
             if (initActivationState != lookupFeatureStatus(featuresService, featureName)) {
@@ -105,11 +107,10 @@ public class FeatureOperation implements OperationBase, Operation<Void, Boolean>
             throw new ConfiguratorException("Internal error");
         }
 
-        return null;
+        return ResultImpl.rollback();
     }
 
-    @Override
-    public Boolean readState() throws ConfiguratorException {
+    Boolean readState() throws ConfiguratorException {
         return lookupFeatureStatus(getFeaturesService(), featureName);
     }
 

configurator/configurator-impl/src/main/java/org/codice/ddf/admin/configurator/impl/ServiceReaderImpl.java
@@ -1,0 +1,61 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.admin.configurator.impl;
+
+import static org.codice.ddf.admin.configurator.impl.OsgiUtils.getBundleContext;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.codice.ddf.admin.configurator.ConfiguratorException;
+import org.codice.ddf.internal.admin.configurator.actions.ServiceReader;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <b> This code is experimental. While this interface is functional and tested, it may change or be
+ * removed in a future version of the library. </b>
+ */
+public class ServiceReaderImpl implements ServiceReader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceReaderImpl.class);
+
+    @Override
+    public <S> S getServiceReference(Class<S> serviceClass) throws ConfiguratorException {
+        BundleContext context = getBundleContext();
+        ServiceReference<S> ref = context.getServiceReference(serviceClass);
+        if (ref == null) {
+            return null;
+        }
+
+        return context.getService(ref);
+    }
+
+    @Override
+    public <S> Set<S> getServices(Class<S> serviceClass, String filter)
+            throws ConfiguratorException {
+        BundleContext context = getBundleContext();
+        try {
+            return context.getServiceReferences(serviceClass, filter)
+                    .stream()
+                    .map(context::getService)
+                    .collect(Collectors.toSet());
+        } catch (InvalidSyntaxException e) {
+            LOGGER.debug("Invalid filter [{}].", filter, e);
+            throw new ConfiguratorException(String.format("Received invalid filter [%s]", filter));
+        }
+    }
+}
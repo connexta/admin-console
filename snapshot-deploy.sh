1ec6ee8a121e00306810b7811322983f7f4c5dd7
tbatie <tbatie@users.noreply.github.com>
2017-06-09 10:08:39 -0700

    DDF-2773: Initial itest environment setup (#77)
    - Created test to check if schema was create
     - Added -q to travis and deploy script

--- snapshot-deploy.sh
+++ snapshot-deploy.sh
@@ -2,7 +2,7 @@
 
 if [ ! -z "$CODICE_PASSWORD" ]; then
   echo "The codice password was available in the environment, deploying snapshots to nexus."
-  mvn clean deploy --settings ./travis_settings.xml
+  mvn clean deploy -q --settings ./travis_settings.xml
 else
   echo "The codice password was not available in the environment, skipping deploy to nexus."
 fi

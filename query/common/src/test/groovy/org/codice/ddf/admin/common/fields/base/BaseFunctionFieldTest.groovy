jefchng/admin-console-beta

DDF-2773: Increase commons test coverage

query/common/src/test/groovy/org/codice/ddf/admin/common/fields/base/BaseFunctionFieldTest.groovy
@@ -1,0 +1,134 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.admin.common.fields.base
+
+import org.codice.ddf.admin.api.DataType
+import org.codice.ddf.admin.api.fields.FunctionField
+import org.codice.ddf.admin.common.fields.base.scalar.StringField
+import org.codice.ddf.admin.common.report.message.DefaultMessages
+import org.codice.ddf.admin.common.report.message.ErrorMessage
+import spock.lang.Specification
+
+class BaseFunctionFieldTest extends Specification {
+
+    BaseFunctionField functionField
+
+    def 'Argument paths are updated with function field name is updated'() {
+        setup:
+        functionField = new TestBaseFunctionField(true)
+
+        expect:
+        functionField.path() == [TestBaseFunctionField.DEFAULT_NAME]
+        functionField.getArguments()[0].path() == [TestBaseFunctionField.DEFAULT_NAME, BaseFunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+
+        when:
+        functionField.fieldName('newName')
+
+        then:
+        functionField.path() == ['newName']
+        functionField.getArguments()[0].path() == ['newName', BaseFunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+    }
+
+    def 'Function field validates arguments'() {
+        setup:
+        functionField = new TestBaseFunctionField(true)
+
+        when:
+        def report = functionField.getValue()
+
+        then:
+        report.messages().size() == 1
+        report.messages()[0].getCode() == DefaultMessages.EMPTY_FIELD
+        report.messages()[0].getPath() == [TestBaseFunctionField.DEFAULT_NAME, BaseFunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+    }
+
+    def 'Adding result message has correct path on added message'() {
+        setup:
+        functionField = new TestBaseFunctionField(false)
+        functionField.addResultMessage(new ErrorMessage('test'))
+
+        when:
+        def report = functionField.getValue()
+
+        then:
+        report.messages().size() == 1
+        report.messages()[0].getPath() == [TestBaseFunctionField.DEFAULT_NAME]
+    }
+
+    def 'Setting function arguments'() {
+        setup:
+        functionField = new TestBaseFunctionField(false)
+
+        expect:
+        functionField.getArguments()[0].getValue() == TestBaseFunctionField.ARG_VALUE
+
+        when:
+        def value = [(StringField.DEFAULT_FIELD_NAME): 'test' ]
+        functionField.setValue(value)
+
+        then:
+        functionField.getArguments()[0].getValue() == 'test'
+    }
+
+    def 'Updating path updates arguments paths'() {
+        setup:
+        functionField = new TestBaseFunctionField(false)
+
+        when:
+        functionField.updatePath(['test'])
+
+        then:
+        functionField.path() == ['test', TestBaseFunctionField.DEFAULT_NAME]
+        functionField.getArguments()[0].path() == ['test', TestBaseFunctionField.DEFAULT_NAME, FunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+    }
+
+    class TestBaseFunctionField extends BaseFunctionField<StringField> {
+
+        static String DEFAULT_NAME = 'testBaseFunctionField'
+
+        static String ARG_VALUE = 'valid'
+
+        StringField stringArg
+
+        TestBaseFunctionField(boolean failValidation) {
+            this(DEFAULT_NAME, failValidation)
+        }
+
+        TestBaseFunctionField(String functionName, boolean failValidation) {
+            super(functionName, 'description', new StringField())
+            stringArg = new StringField()
+            if(failValidation) {
+                stringArg.setValue('')
+            } else {
+                stringArg.setValue(ARG_VALUE)
+            }
+            updateArgumentPaths()
+        }
+
+        @Override
+        List<DataType> getArguments() {
+            return Collections.singletonList(stringArg)
+        }
+
+        @Override
+        FunctionField<StringField> newInstance() {
+            return new TestBaseFunctionField(DEFAULT_NAME)
+        }
+
+        @Override
+        StringField performFunction() {
+            return new StringField('result')
+        }
+    }
+}

query/common/src/test/groovy/org/codice/ddf/admin/common/fields/common/HostFieldTest.groovy
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.admin.common.fields.common
+
+import org.codice.ddf.admin.common.report.message.DefaultMessages
+import spock.lang.Specification
+
+class HostFieldTest extends Specification {
+
+    HostField hostField
+
+    static PORT_FIELD_PATH = [HostField.DEFAULT_FIELD_NAME, PortField.DEFAULT_FIELD_NAME]
+
+    static HOSTFIELD_FIELD_PATH = [HostField.DEFAULT_FIELD_NAME, HostnameField.DEFAULT_FIELD_NAME]
+
+    def setup() {
+        hostField = new HostField()
+    }
+
+    def 'Fail validation when missing required fields'() {
+        setup:
+        hostField.isRequired(true)
+
+        when:
+        def validationMsgs = hostField.validate()
+
+        then:
+        validationMsgs.size() == 2
+        validationMsgs.count {
+            it.getCode() == DefaultMessages.MISSING_REQUIRED_FIELD
+        } == 2
+        validationMsgs*.getPath() == [HOSTFIELD_FIELD_PATH, PORT_FIELD_PATH]
+    }
+
+    def 'Missing required port field when host is required and hostname is provided'() {
+        setup:
+        hostField.isRequired(true)
+        hostField.name('localhost')
+
+        when:
+        def validationMsgs = hostField.validate()
+
+        then:
+        validationMsgs.size() == 1
+        validationMsgs.get(0).getCode() == DefaultMessages.MISSING_REQUIRED_FIELD
+        validationMsgs.get(0).getPath() == PORT_FIELD_PATH
+    }
+
+    def 'Missing required hostname field when host is required and port is provided'() {
+        setup:
+        hostField.isRequired(true)
+        hostField.port(8993)
+
+        when:
+        def validationMsgs = hostField.validate()
+
+        then:
+        validationMsgs.size() == 1
+        validationMsgs.get(0).getCode() == DefaultMessages.MISSING_REQUIRED_FIELD
+        validationMsgs.get(0).getPath() == HOSTFIELD_FIELD_PATH
+    }
+}

connexta/admin-console

query/common/src/test/groovy/org/codice/ddf/admin/common/fields/base/BaseFunctionFieldTest.groovy
@@ -1,0 +1,134 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.admin.common.fields.base
+
+import org.codice.ddf.admin.api.DataType
+import org.codice.ddf.admin.api.fields.FunctionField
+import org.codice.ddf.admin.common.fields.base.scalar.StringField
+import org.codice.ddf.admin.common.report.message.DefaultMessages
+import org.codice.ddf.admin.common.report.message.ErrorMessage
+import spock.lang.Specification
+
+class BaseFunctionFieldTest extends Specification {
+
+    BaseFunctionField functionField
+
+    def 'Argument paths are updated when function field name is updated'() {
+        setup:
+        functionField = new TestBaseFunctionField(true)
+
+        expect:
+        functionField.path() == [TestBaseFunctionField.DEFAULT_NAME]
+        functionField.getArguments()[0].path() == [TestBaseFunctionField.DEFAULT_NAME, BaseFunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+
+        when:
+        functionField.fieldName('newName')
+
+        then:
+        functionField.path() == ['newName']
+        functionField.getArguments()[0].path() == ['newName', BaseFunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+    }
+
+    def 'Function field validates arguments'() {
+        setup:
+        functionField = new TestBaseFunctionField(true)
+
+        when:
+        def report = functionField.getValue()
+
+        then:
+        report.messages().size() == 1
+        report.messages()[0].getCode() == DefaultMessages.EMPTY_FIELD
+        report.messages()[0].getPath() == [TestBaseFunctionField.DEFAULT_NAME, BaseFunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+    }
+
+    def 'Adding result message has correct path on added message'() {
+        setup:
+        functionField = new TestBaseFunctionField(false)
+        functionField.addResultMessage(new ErrorMessage('test'))
+
+        when:
+        def report = functionField.getValue()
+
+        then:
+        report.messages().size() == 1
+        report.messages()[0].getPath() == [TestBaseFunctionField.DEFAULT_NAME]
+    }
+
+    def 'Setting function arguments'() {
+        setup:
+        functionField = new TestBaseFunctionField(false)
+
+        expect:
+        functionField.getArguments()[0].getValue() == TestBaseFunctionField.ARG_VALUE
+
+        when:
+        def value = [(StringField.DEFAULT_FIELD_NAME): 'test' ]
+        functionField.setValue(value)
+
+        then:
+        functionField.getArguments()[0].getValue() == 'test'
+    }
+
+    def 'Updating path updates arguments paths'() {
+        setup:
+        functionField = new TestBaseFunctionField(false)
+
+        when:
+        functionField.updatePath(['test'])
+
+        then:
+        functionField.path() == ['test', TestBaseFunctionField.DEFAULT_NAME]
+        functionField.getArguments()[0].path() == ['test', TestBaseFunctionField.DEFAULT_NAME, FunctionField.ARGUMENT, StringField.DEFAULT_FIELD_NAME]
+    }
+
+    class TestBaseFunctionField extends BaseFunctionField<StringField> {
+
+        static String DEFAULT_NAME = 'testBaseFunctionField'
+
+        static String ARG_VALUE = 'valid'
+
+        StringField stringArg
+
+        TestBaseFunctionField(boolean failValidation) {
+            this(DEFAULT_NAME, failValidation)
+        }
+
+        TestBaseFunctionField(String functionName, boolean failValidation) {
+            super(functionName, 'description', new StringField())
+            stringArg = new StringField()
+            if(failValidation) {
+                stringArg.setValue('')
+            } else {
+                stringArg.setValue(ARG_VALUE)
+            }
+            updateArgumentPaths()
+        }
+
+        @Override
+        List<DataType> getArguments() {
+            return Collections.singletonList(stringArg)
+        }
+
+        @Override
+        FunctionField<StringField> newInstance() {
+            return new TestBaseFunctionField(DEFAULT_NAME)
+        }
+
+        @Override
+        StringField performFunction() {
+            return new StringField('result')
+        }
+    }
+}

query/common/src/test/groovy/org/codice/ddf/admin/common/fields/common/HostFieldTest.groovy
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.admin.common.fields.common
+
+import org.codice.ddf.admin.common.report.message.DefaultMessages
+import spock.lang.Specification
+
+class HostFieldTest extends Specification {
+
+    HostField hostField
+
+    static PORT_FIELD_PATH = [HostField.DEFAULT_FIELD_NAME, PortField.DEFAULT_FIELD_NAME]
+
+    static HOSTFIELD_FIELD_PATH = [HostField.DEFAULT_FIELD_NAME, HostnameField.DEFAULT_FIELD_NAME]
+
+    def setup() {
+        hostField = new HostField()
+    }
+
+    def 'Fail validation when missing required fields'() {
+        setup:
+        hostField.isRequired(true)
+
+        when:
+        def validationMsgs = hostField.validate()
+
+        then:
+        validationMsgs.size() == 2
+        validationMsgs.count {
+            it.getCode() == DefaultMessages.MISSING_REQUIRED_FIELD
+        } == 2
+        validationMsgs*.getPath() == [HOSTFIELD_FIELD_PATH, PORT_FIELD_PATH]
+    }
+
+    def 'Missing required port field when host is required and hostname is provided'() {
+        setup:
+        hostField.isRequired(true)
+        hostField.hostname('localhost')
+
+        when:
+        def validationMsgs = hostField.validate()
+
+        then:
+        validationMsgs.size() == 1
+        validationMsgs.get(0).getCode() == DefaultMessages.MISSING_REQUIRED_FIELD
+        validationMsgs.get(0).getPath() == PORT_FIELD_PATH
+    }
+
+    def 'Missing required hostname field when host is required and port is provided'() {
+        setup:
+        hostField.isRequired(true)
+        hostField.port(8993)
+
+        when:
+        def validationMsgs = hostField.validate()
+
+        then:
+        validationMsgs.size() == 1
+        validationMsgs.get(0).getCode() == DefaultMessages.MISSING_REQUIRED_FIELD
+        validationMsgs.get(0).getPath() == HOSTFIELD_FIELD_PATH
+    }
+}
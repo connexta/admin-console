14dcf5f6060c5b57641036c96d155f879d75d352
R.A. Porter <coyotesqrl@gmail.com>
2017-06-20 16:46:33 -0400

    DDF-2773 Inlines uri validation and removes dependency on commons validator. (#87)

--- query/common/src/main/java/org/codice/ddf/admin/common/fields/common/ContextPath.java
+++ query/common/src/main/java/org/codice/ddf/admin/common/fields/common/ContextPath.java
@@ -15,9 +15,11 @@ package org.codice.ddf.admin.common.fields.common;
 
 import static org.codice.ddf.admin.common.report.message.DefaultMessages.invalidContextPathError;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.regex.Pattern;
 
-import org.apache.commons.validator.UrlValidator;
 import org.codice.ddf.admin.api.report.ErrorMessage;
 import org.codice.ddf.admin.common.fields.base.scalar.StringField;
 
@@ -65,10 +67,53 @@ public class ContextPath extends StringField {
         return this;
     }
 
-    private static class UriPathValidator extends UrlValidator {
-        @Override
-        protected boolean isValidPath(String path) {
-            return super.isValidPath(path);
+    private static class UriPathValidator {
+        private static final Pattern PATH_PATTERN = Pattern.compile(
+                "^(/[-\\w:@&?=+,.!/~*'%$_;\\(\\)]*)?$");
+
+        /**
+         * Returns true if the path is valid.  A <code>null</code> value is considered invalid.
+         *
+         * @param path Path value to validate.
+         * @return true if path is valid.
+         */
+        private boolean isValidPath(String path) {
+            if (path == null) {
+                return false;
+            }
+
+            if (!PATH_PATTERN.matcher(path)
+                    .matches()) {
+                return false;
+            }
+
+            try {
+                URI uri = new URI(null, null, path, null);
+                String norm = uri.normalize()
+                        .getPath();
+                if (norm.startsWith("/../") // Trying to go via the parent dir
+                        || norm.equals("/..")) {   // Trying to go to the parent dir
+                    return false;
+                }
+            } catch (URISyntaxException e) {
+                return false;
+            }
+
+            int slash2Count = countToken(path);
+            return !(slash2Count > 0);
+        }
+
+        private int countToken(String target) {
+            int tokenIndex = 0;
+            int count = 0;
+            while (tokenIndex != -1) {
+                tokenIndex = target.indexOf("//", tokenIndex);
+                if (tokenIndex > -1) {
+                    tokenIndex++;
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }

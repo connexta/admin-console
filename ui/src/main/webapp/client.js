d891026774a66bd7265d9dbf75ce811c46a56721
Chris Badahdah <a.s.badahdah@gmail.com>
2017-10-04 12:07:51 -0700

    Auto remove __typename from variables.

--- ui/src/main/webapp/client.js
+++ ui/src/main/webapp/client.js
@@ -1,5 +1,6 @@
 import { ApolloClient } from 'react-apollo'
 import { createBatchingNetworkInterface } from 'apollo-client'
+import { Map, List, fromJS } from 'immutable'
 
 const reduxRootSelector = (state) => state.get('apollo')
 const networkInterface = createBatchingNetworkInterface({
@@ -9,6 +10,30 @@ const networkInterface = createBatchingNetworkInterface({
     credentials: 'same-origin'
   }
 })
+
+const stripTypename = (obj) => {
+  if (Map.isMap(obj)) {
+    return obj.remove('__typename').map(stripTypename)
+  } else if (List.isList(obj)) {
+    return obj.map(stripTypename)
+  } else {
+    return obj
+  }
+}
+
+const middleware = {
+  applyBatchMiddleware ({ requests }, next) {
+    requests.forEach(({ variables }, i) => {
+      const stripped = stripTypename(fromJS(variables))
+      if (Map.isMap(stripped) || List.isList(stripped)) {
+        requests[i].variables = stripped.toJS()
+      }
+    })
+    next()
+  }
+}
+
+networkInterface.use([middleware])
 
 export default new ApolloClient({
   networkInterface,

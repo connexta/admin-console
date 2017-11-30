37e2b49376cedb766df7fc926a53dfc565e31eaf
Blen Desta <blen.desta@connexta.com>
2017-11-30 09:47:37 -0700

    DDF-2773 Changed @VisibleForTestingAnnotation to a suppression

--- query/sources/impl/src/main/java/org/codice/ddf/admin/sources/csw/CswSourceUtils.java
+++ query/sources/impl/src/main/java/org/codice/ddf/admin/sources/csw/CswSourceUtils.java
@@ -21,7 +21,6 @@ import static org.codice.ddf.admin.sources.fields.CswProfile.DDFCswFederatedSour
 import static org.codice.ddf.admin.sources.fields.CswProfile.GmdCswFederatedSource.GMD_CSW_ISO_FEDERATED_SOURCE;
 import static org.codice.ddf.admin.sources.utils.SourceUtilCommons.SOURCES_NAMESPACE_CONTEXT;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import java.util.ArrayList;
@@ -221,7 +220,8 @@ public class CswSourceUtils {
     return Reports.from(unknownEndpointError(responseField.requestUrlField().getPath()));
   }
 
-  @VisibleForTesting
+  @SuppressWarnings(
+      "squid:UnusedPrivateMethod" /* For testing purposes only. Groovy can access private methods. */)
   private void setRequestUtils(RequestUtils requestUtils) {
     this.requestUtils = requestUtils;
   }
